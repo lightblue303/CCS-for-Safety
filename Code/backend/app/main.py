f87ed73d96d6b2c750016a1a48d66daede0ca7e7
leedw0109 <240190605+leedw0109@users.noreply.github.com>
2026-02-11 16:37:37 +0900

    Add docker setup and update main

--- Code/backend/app/main.py
+++ Code/backend/app/main.py
@@ -4,9 +4,12 @@ from app.api.v1.routers.events import router as events_router
 from app.api.v1.routers.notifications import router as notifications_router
 from app.api.v1.routers.push_tokens import router as push_tokens_router
 from app.api.v1.routers.subscriptions import router as subscriptions_router
+from app.core.database import Base, engine
 
 app = FastAPI(title="Control Backend")
-
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 app.include_router(events_router)
 
 app.include_router(notifications_router)
